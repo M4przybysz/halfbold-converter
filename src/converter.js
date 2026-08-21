510972b5d55dad7f16f024253e1ee92d5ca6442d
Maciej <maciej.przybyl.05@gmail.com>
2026-08-21 21:47:53 +0200

    Base JS + small tweaks to HTML and CSS

--- src/converter.js
+++ src/converter.js
@@ -2,3 +2,27 @@
     Copyright (c) 2026 Maciej Przybył, Alina Osińska
     Licensed under the MIT License. See LICENSE file.
 */
+
+// 
+const InputTextType = Object.freeze({
+    PLAIN_TEXT : "PLAIN_TEXT",
+    HTML : "HTML",
+    MARKDOWN : "MARKDOWN"
+})
+
+// 
+function outputBoldingPercentage(value) {
+    document.getElementById('boldingPercentageOutput').textContent = Math.round(value * 100) + '%'
+}
+
+// Converter functions
+function convertText() {
+    // Converter arguments
+    let inputText = document.getElementById("inputText").value // Text to convert
+    let inputType = InputTextType[document.getElementById("inputType").value] // Type of inputed text (plain text/HTML/Markdown)
+    let boldingPercentage = document.getElementById("boldingPercentage").value // How much of each word is supposed to be bold (range 5-75%)
+    let markingColor = document.getElementById("markingColor").value // Color of text that is already bolded in HTML and Markdown (hex color)
+
+    // Log arguments
+    console.log(inputText, inputType, boldingPercentage, markingColor)
+}
