5e7f1a1625f983865eb43c1cce87b94530eaec19
Maciej <maciej.przybyl.05@gmail.com>
2026-09-23 12:36:48 +0200

    Removed copyright and license notice from converter source files

--- docs/converter.js
+++ docs/converter.js
@@ -1,8 +1,3 @@
-/* 
-    Copyright (c) 2026 Maciej Przybył, Alina Osińska
-    Licensed under the MIT License. See LICENSE file.
-*/
-
 // Clamp function
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 
